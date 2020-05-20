0fab9985ad59567d9722649ea9ea33fea5ff97b3
ChaseSully222 <Jcsullins222@yahoo.com>
2020-05-20 15:51:23 -0500

    initial commit Chapter 1 installations

---
+++ KennywoodEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/chase/backend/workspace/python/kennywood/KennywoodEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
